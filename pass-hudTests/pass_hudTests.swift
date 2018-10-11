f753f1911c74027fb38791c8fb67476f49230b07
mnussbaum <michaelnussbaum08@gmail.com>
2018-10-10 23:12:06 -0700

    Initial Commit

---
+++ pass-hudTests/pass_hudTests.swift
@@ -0,0 +1,34 @@
+//
+//  pass_hudTests.swift
+//  pass-hudTests
+//
+//  Created by Nussbaum, Michael on 10/10/18.
+//  Copyright © 2018 mnussbaum. All rights reserved.
+//
+
+import XCTest
+@testable import pass_hud
+
+class pass_hudTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
